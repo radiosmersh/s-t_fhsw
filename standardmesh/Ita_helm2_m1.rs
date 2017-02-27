cc90562e1d0b1edeaf72b8eec0c481e204e997aa
radiosmersh <radiosmersh@gmail.com>
2017-02-27 14:10:12 +0300

    First commit

---
+++ standardmesh/Ita_helm2_m1.rs
@@ -0,0 +1,24 @@
+subshader "itHelmetare_m1_Material0" "StandardMesh/Default"
+{
+	lighting true;
+	lightingSpecular false;
+	materialDiffuse 1 1 1;
+	texture "texture/italhelm2";
+}
+
+subshader "itHelmetair_m1_Material1" "StandardMesh/Default"
+{
+	lighting true;
+	lightingSpecular false;
+	materialDiffuse 1 1 1;
+	texture "texture/headita";
+}
+
+subshader "itHelmetare_m1_Material2" "StandardMesh/Default"
+{
+	lighting true;
+	lightingSpecular false;
+	materialDiffuse 1 1 1;
+	texture "texture/M38Helmet";
+}
+
---
+++ standardmesh/ita_backpack2_m1.rs
@@ -0,0 +1,8 @@
+subshader "ita_backpack2_m1_1" "StandardMesh/Default"
+{
+	lighting true;
+	lightingSpecular false;
+	materialDiffuse 1 1 1;
+	texture "texture/Itaequip";
+}
+
