b15e05b04c226136cadd8101cf33fa8288ed4b1d
radiosmersh <radiosmersh@gmail.com>
2018-07-18 00:43:41 +0300

    crazygamelover's contribution:
    New textures:
    UK
    -desert Piper Cub (FH2 texture)
    -night Europe uniforms
    -night No4
    USA
    -night P51H
    USSR
    -night T-44
    -winter T-28
    -Russian M4A1 sherman
    Hungary
    -darker 35M
    France
    -darker Lebel wood
    Germany
    -night Stg44
    -night K98
    -desert Marder II
    -captured R35
    Finland
    -winter wz36 AT gun
    
    New mapfiles:
    -Bastogne
    -Battle of Britain mod
    -Desert Hill Day 1
    -FHT Golf Hotel
    -FHT Sittang Bridge
    -Kota Bharu
    -Operation Hailstone mod
    -Operation Kikusui day 2 mod
    -Operation Lilliput
    -Spurning Fate
    -Stashuv Area
    -The Attack on Carentan

---
+++ bf1942/levels/The_Attack_on_Carentan-1944/StandardMesh/M1Carbine.rs
@@ -0,0 +1,16 @@
+subshader "M1Carbine_para_Material0" "StandardMesh/Default"
+{
+	lighting true;
+	lightingSpecular false;
+	materialDiffuse 1.0 1.0 1.0;
+	texture "texture/M1carbine";
+}
+
+subshader "M1Carbine_para_Material1" "StandardMesh/Default"
+{
+	lighting true;
+	lightingSpecular false;
+	materialDiffuse 1.0 1.0 1.0;
+	texture "texture/M1carbine_stock";
+}
+
